ee8c9da10104ff751a0dc7186e88622992e14fbc
Xinbo Wu <wuxinbo@us.ibm.com>
2019-10-08 16:30:12 -0700

    clean up code

--- ModeratorsExplorer/Controllers/ModeratorsListViewController.swift
+++ ModeratorsExplorer/Controllers/ModeratorsListViewController.swift
@@ -1,6 +1,8 @@
 
 import UIKit
 
+// MARK: - ModeratorsListViewController
+
 class ModeratorsListViewController: UIViewController, AlertDisplayer {
   private enum CellIdentifiers {
     static let list = "List"
@@ -19,9 +21,8 @@ class ModeratorsListViewController: UIViewController, AlertDisplayer {
     super.viewDidLoad()
     
     indicatorView.color = ColorPalette.RWGreen
-    indicatorView.startAnimating()
     
-    tableView.isHidden = true
+    tableView.tableFooterView = indicatorView
     tableView.separatorColor = ColorPalette.RWGreen
     tableView.dataSource = self
     tableView.prefetchDataSource = self
@@ -33,6 +34,19 @@ class ModeratorsListViewController: UIViewController, AlertDisplayer {
   }
 }
 
+private extension ModeratorsListViewController {
+  func isLoadingCell(for indexPath: IndexPath) -> Bool {
+    return indexPath.row >= viewModel.currentCount
+  }
+  
+  func visibleIndexPathsToReload(intersecting indexPaths: [IndexPath]) -> [IndexPath] {
+    let indexPathsForVisibleRows = tableView.indexPathsForVisibleRows ?? []
+    let indexPathsIntersection = Set(indexPathsForVisibleRows).intersection(indexPaths)
+    return Array(indexPathsIntersection)
+  }
+}
+
+// MARK: - UITableViewDataSource
 extension ModeratorsListViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return viewModel.totalCount
@@ -52,26 +66,7 @@ extension ModeratorsListViewController: UITableViewDataSource {
   }
 }
 
-extension ModeratorsListViewController: ModeratorsViewModelDelegate {
-  func onFetchCompleted(with newIndexPathsToReload: [IndexPath]?) {
-    guard let newIndexPathsToReload = newIndexPathsToReload else {
-      indicatorView.stopAnimating()
-      tableView.isHidden = false
-      tableView.reloadData()
-      return
-    }
-    let indexPathsToReload = visibleIndexPathsToReload(intersecting: newIndexPathsToReload)
-    tableView.reloadRows(at: indexPathsToReload, with: .automatic)
-  }
-  
-  func onFetchFailed(with reason: String) {
-    indicatorView.stopAnimating()
-    
-    let title = "Warning".localizedString
-    let action = UIAlertAction(title: "OK".localizedString, style: .default)
-    displayAlert(with: title , message: reason, actions: [action])
-  }
-}
+// MARK: -  UITableViewDataSourcePrefetching
 
 extension ModeratorsListViewController: UITableViewDataSourcePrefetching {
   func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
@@ -81,14 +76,27 @@ extension ModeratorsListViewController: UITableViewDataSourcePrefetching {
   }
 }
 
-private extension ModeratorsListViewController {
-  func isLoadingCell(for indexPath: IndexPath) -> Bool {
-    return indexPath.row >= viewModel.currentCount
+// MARK: - ModeratorsViewModelDelegate
+
+extension ModeratorsListViewController: ModeratorsViewModelDelegate {
+  func onFetchCompleted(with newIndexPathsToReload: [IndexPath]?) {
+    guard let newIndexPathsToReload = newIndexPathsToReload else {
+      tableView.tableFooterView = nil
+      tableView.reloadData()
+      return
+    }
+    let indexPathsToReload = visibleIndexPathsToReload(intersecting: newIndexPathsToReload)
+    tableView.reloadRows(at: indexPathsToReload, with: .automatic)
   }
   
-  func visibleIndexPathsToReload(intersecting indexPaths: [IndexPath]) -> [IndexPath] {
-    let indexPathsForVisibleRows = tableView.indexPathsForVisibleRows ?? []
-    let indexPathsIntersection = Set(indexPathsForVisibleRows).intersection(indexPaths)
-    return Array(indexPathsIntersection)
+  func onFetchFailed(with reason: String) {
+    tableView.tableFooterView = nil
+    tableView.reloadData()
+    
+    let title = "Warning".localizedString
+    let action = UIAlertAction(title: "OK".localizedString, style: .default)
+    displayAlert(with: title , message: reason, actions: [action])
   }
 }
+
+
